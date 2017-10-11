61288c6dc357fac77561e2fc2d44073b92c4fece
Josh <joshuakettle1990@gmail.com>
2017-10-11 04:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function happyHolidays(){
-  return 'Happy holidays!'
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays, ${name}!'
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!'
+  return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown(days, holiday){
